7197254c9ada617aa4d7e0cc7e2d6062511c1503
Alok_ Amar (amaalok@deloitte.com) <amaalok@your.hostname.com>
2022-05-17 20:22:46 +0530

    added internationalization

--- src/Component/Login/Login.tsx
+++ src/Component/Login/Login.tsx
@@ -1,41 +1,53 @@
 import React from 'react';
-import { Button, Dropdown, Form } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { Button, Form } from 'react-bootstrap';
 import Tracker from '../../assests/images/Icon.png';
 import SideImg from '../../assests/images/sideImg.png';
 import classes from './Login.module.css';
+import Language from '../Language/Language';
+
 const Login = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={classes.login}>
       <div className={classes.sidebar}>
         <img src={Tracker} className={classes.tracker} />
         <img src={SideImg} className={classes.tracker} />
         <div>
-          <Dropdown>
+          <Language />
+          {/* <Dropdown>
             <Dropdown.Toggle variant="dark" id="dropdown-basic" className={classes.dropdown}>
-              LANGUAGE
+              {t('LANGUAGE')}
             </Dropdown.Toggle>
-
             <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">English</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Hindi</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">French</Dropdown.Item>
+              {languages.map(({ code, name, country_code }) => (
+                <Dropdown.Item key={country_code}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => i18next.changeLanguage(code)}
+                  key={country_code}>
+                  {name}
+                </button>
+                </Dropdown.Item>
+              ))}
             </Dropdown.Menu>
-          </Dropdown>
+          </Dropdown> */}
         </div>
       </div>
       <div className={classes['login_section']}>
         <Form className={classes.form}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter your email address" />
+            <Form.Label>{t('Email')}</Form.Label>
+            <Form.Control type="email" placeholder={t('Enter your email address')} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>{t('Password')}</Form.Label>
             <Form.Control type="password" placeholder="**********" />
           </Form.Group>
           <Button variant="dark" className={classes.button} type="submit">
-            LOGIN
+            {t('LOGIN')}
           </Button>
         </Form>
       </div>
