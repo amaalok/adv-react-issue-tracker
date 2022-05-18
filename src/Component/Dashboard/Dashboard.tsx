75c9860c229faf7745b2f6b9c902f14e41a9d2dc
Alok_ Amar (amaalok@deloitte.com) <amaalok@your.hostname.com>
2022-05-18 19:45:35 +0530

    extract assignee from issue

--- src/Component/Dashboard/Dashboard.tsx
+++ src/Component/Dashboard/Dashboard.tsx
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavBar from '../NavBar/NavBar';
 import Sidebar from '../Sidebar/Sidebar';
+import Card from '../Card/Card';
 import classes from './Dashboard.module.css';
 
 const Dashboard = () => {
   const [allProjects, setAllProjects] = useState([]);
   const [user, setUser] = useState([]);
+  const [project, setProject] = useState('');
+  const [issues, setIssues] = useState([] as any[]);
+  const [assignees, setAssignees] = useState([]);
   const headers: any = {
     userID: localStorage.getItem('userId') !== null ? localStorage.getItem('userId') : '1'
   };
@@ -18,9 +22,7 @@ const Dashboard = () => {
         )}`,
         { headers: headers }
       );
-      // console.log(response.data[0].name);
       setUser(response.data[0].name);
-      // console.log('user', user);
     }
     getUser();
   }, []);
@@ -36,6 +38,57 @@ const Dashboard = () => {
     }
     getRes();
   }, []);
+
+  useEffect(() => {
+    async function getIssues() {
+      const response = await axios.get(
+        `https://hu-22-angular-mockapi-urtjok3rza-wl.a.run.app/issue?projectID=${project}`,
+        { headers: headers }
+      );
+      setIssues(response.data);
+    }
+    getIssues();
+  }, [project]);
+  // interface issue{
+  //       "id": "HU00000175",
+  //     "summary": "Create mock API 86",
+  //     "type": 2,
+  //     "projectID": "P1100000",
+  //     "description": "create mock api for angular track",
+  //     "priority": 1,
+  //     "assignee": {
+  //         "id": 12,
+  //         "name": "Test user",
+  //         "email": "test@test.com",
+  //         "teamName": "Hashers",
+  //         "desination": "Test"
+  //     },
+  //     "tags": [
+  //         "HU-22",
+  //         "Angular track"
+  //     ],
+  //     "sprint": "Sprint 3",
+  //     "storyPoint": 5,
+  //     "status": 1,
+  //     "createdBy": {
+  //         "id": 1,
+  //         "name": "Akash Garg",
+  //         "email": "akagarg@deloitte.com",
+  //         "teamName": "Hashers",
+  //         "desination": "Track Helper"
+  //     },
+  //     "createdOn": "2022-04-21T00:00:00.000Z",
+  //     "updatedBy": null,
+  //     "updatedOn": null
+  // },
+  useEffect(() => {
+    // async function getIssues() {
+    //   const response = await axios.get(
+    //     `https://hu-22-angular-mockapi-urtjok3rza-wl.a.run.app/issue?proj
+    const assign = [...issues];
+    assign.map((x) => x.assignee);
+    console.log(assign);
+  }, [issues]);
   if (allProjects.length <= 0) {
     return <h1>Hello</h1>;
   }
@@ -51,7 +104,12 @@ const Dashboard = () => {
           </div>
           <div className={classes['input-container']}>
             <label htmlFor={classes.select}>Project Name</label>
-            <select className={classes.select} name="Priority">
+            <select
+              className={classes.select}
+              name="Priority"
+              onChange={(event) => {
+                setProject(event.target.value);
+              }}>
               <option selected disabled hidden>
                 Select
               </option>
@@ -86,6 +144,7 @@ const Dashboard = () => {
               <option value="2">MEDIUM</option>
               <option value="3">HIGH</option>
             </select>
+            <Card />
           </div>
         </div>
       </div>
